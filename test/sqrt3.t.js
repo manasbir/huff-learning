146e7a1d838ce0d7f34fb75e0ccca6f17c269355
manas <bagrimanasbir@gmail.com>
2022-06-22 22:04:20 -0700

    IT WORKSSSSS

--- test/sqrt3.t.js
+++ test/sqrt3.t.js
@@ -4,7 +4,7 @@ const { ethers } = require("hardhat");
 
 let sqrt3;
 
-describe("sqrt tests", function () {
+describe("sqrt3 tests", function () {
   beforeEach(async function () {
     const Sqrt3 = await ethers.getContractFactory("sqrt3");
     sqrt3 = await Sqrt3.deploy();
@@ -16,9 +16,12 @@ describe("sqrt tests", function () {
     );
   });
     it("4", async function () {
-    expect(await sqrt3.sqrt(5)).to.equal(3);
+    expect(await sqrt3.sqrt(4)).to.equal(2);
   });
   it("9", async function () {
-    expect(await sqrt3.sqrt(9)).to.equal(5);
+    expect(await sqrt3.sqrt(9)).to.equal(3);
+  });
+  it("151", async function () {
+    expect(await sqrt3.sqrt(151)).to.equal(12);
   });
 });
